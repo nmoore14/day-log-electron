43bdae75d4e5a2bc90ac90308f3458b6fa8fbde2
Nick Moore <nickmoore_1@live.com>
2018-11-13 13:19:11 -0500

    figuring out how to make a progress circle.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,20 @@
   <div class="bg-black text-white flex-1 ml-56 p-3">
     <div class="flex-1">
       <h1 class="text-5xl text-center font-sans font-thin">Welcome</h1>
+      <div class="flex bg-grey-lighter">
+        <todos-progress></todos-progress>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import TodoProgress from '../components/TodoProgress.vue';
 // @ is an alias to /src
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    TodoProgress
+  }
 };
 </script>
