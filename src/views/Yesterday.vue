d9deeb516168182c57f51e259e84d57ced8aeb88
Nick Moore <nickmoore_1@live.com>
2018-11-13 07:11:41 -0500

    Adding more views

---
+++ src/views/Yesterday.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-black text-white flex-1 ml-56 p-3">
+    <div class="flex-1">
+      <h1 class="text-5xl text-center font-sans font-thin">Yesterday</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "yesterday"
+};
+</script>
