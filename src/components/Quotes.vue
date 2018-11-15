45e3da7214611c6da05a558b7e1efd7e842af3e0
Nick Moore <nickmoore_1@live.com>
2018-11-15 14:27:50 -0500

    Trying to figure out how to use axios and vuex with this project

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,21 +1,28 @@
 <template>
 	<div class="bg-grey-darkest rounded text-center pt-10 ml-2">
 		<h1 class="font-sans font-hairline">Quote of the day:</h1>
-		<h3 class="font-sans font-thin text-5xl">{{ myQuote.content }}</h3>
-		<p class="font-sans text-xl font-thin">~ {{ myQuote.author }}</p>
+		<ul>
+      <li v-for="(quote, index) in quotes" :key="index">{{ quote.content }}</li>
+    </ul>
 	</div>	
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   data() {
     return {
-      myQuote: {
-        content:
-          "Sometimes life hits you in the head with a brick. Don't loose faith.",
-        author: "Steve Jobs"
-      }
+      quoteContent: '',
+      quoteAuthor: '',
+      quoteIndex: 0
     };
-  }
+  },
+  mounted() {
+    this.$store.dispatch('loadQuotes');
+  },
+  computed: mapState([
+    'quotes'
+  ])
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.use(Vuex);
+Vue.use(VueAxios, axios);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    quotes: []
+  },
+  mutations: {
+    SET_QUOTES(state, quotes) {
+      state.quotes = quotes;
+    }
+  },
+  actions: {
+    loadQuotes: ({ commit }) => {
+      axios
+        .get("/inspQuotes.json")
+        .then(r => r.data)
+        .then(quotes => {
+          commit('SET_QOUTES', quotes)
+        })
+    }
+  }
 });
