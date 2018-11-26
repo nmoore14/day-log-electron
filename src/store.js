a87c9ff30ca9b015a5afedecc5df7e13e98b16be
Nick Moore <nickmoore_1@live.com>
2018-11-26 12:15:36 -0500

    removing data in the store.js file

--- src/store.js
+++ src/store.js
@@ -5,24 +5,3 @@ import VueAxios from "vue-axios";
 
 Vue.use(Vuex);
 Vue.use(VueAxios, axios);
-
-export default new Vuex.Store({
-  state: {
-    quotes: []
-  },
-  mutations: {
-    SET_QUOTES(state, quotes) {
-      state.quotes = quotes;
-    }
-  },
-  actions: {
-    loadQuotes: ({ commit }) => {
-      axios
-        .get("/inspQuotes.json")
-        .then(r => r.data)
-        .then(quotes => {
-          commit('SET_QOUTES', quotes)
-        })
-    }
-  }
-});
