788079aea6977168cd5414fcebb4002b487b1bae
Nick Moore <nickmoore_1@live.com>
2018-12-14 09:53:12 -0500

    Reverted back to beginning of day, working on the todo view and comps

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -8,6 +8,14 @@
 
 <script>
 // @ is an alias to /src
+
+// var now = new Date();
+// var start = new Date(now.getFullYear(), 0, 0);
+// var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+// var oneDay = 1000 * 60 * 60 * 24;
+// var day = Math.floor(diff / oneDay);
+// console.log('Day of year: ' + day);
+
 export default {
   name: "todos"
 };
