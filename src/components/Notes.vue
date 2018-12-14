78029a3d1cf0f658f3544294e3e1aebd722d2f41
Nick Moore <nickmoore_1@live.com>
2018-12-14 08:56:31 -0500

    Adding note functionality along with Toastr notifications.

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,10 +1,78 @@
 <template>
-	<div class="flex w-2/3 p-3">
-		<div class="flex">
-			<h3 class="text-left font-sans font-thin">Notes:</h3>
-		</div>
-		<div class="flex mt-7 -ml-12">
-			<textarea name="notes_home" id="notes-home" cols="112" rows="15" class="bg-grey rounded"></textarea>
-		</div>
-	</div>
+  <div class="flex-col p-3">
+    <div class="flex-1">
+      <h3 class="text-left font-sans font-thin">Notes:</h3>
+    </div>
+    <div class="flex-1">
+      <textarea
+        name="notes_home"
+        id="notes-home"
+        cols="112"
+        rows="12"
+        class="bg-grey-light font-sans font-thin text-xl rounded p-2"
+        v-model="newNote.content"
+      ></textarea>
+    </div>
+    <div class="flex-1 w-full h-12">
+      <button
+        class="h-12 w-full bg-teal rounded text-white font-sans font-thin hover:bg-teal-darker"
+        @click="addNote()"
+      >Add Note</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import { db } from "../config/db";
+import { mapMutations } from "vuex";
+import toastr from "toastr";
+
+let notesRef = db.ref("notes");
+
+export default {
+  firebase: {
+    notes: notesRef
+  },
+  data() {
+    return {
+      newNote: {
+        content: "",
+        noteDate: ""
+      },
+      notesStored: this.notesRef
+    };
+  },
+  methods: {
+    ...mapMutations(["ADD_NOTE", "GET_NOTES"]),
+    mergeNotes() {
+      this.GET_NOTES(this.notesStored);
+    },
+    addNote() {
+      var d = new Date();
+      var month = d.getMonth();
+      var day = d.getDay();
+      var year = d.getFullYear();
+      if (this.newNote.content != "") {
+        this.newNote.noteDate = month + "-" + day + "-" + year;
+        notesRef.push(this.newNote);
+        this.ADD_NOTE(this.newNote);
+        toastr.success("Note added successfully!");
+        this.newNote.content = "";
+        this.newNote.noteDate = "";
+      }
+    }
+  },
+  mounted() {
+    if (this.notes.length > 0) {
+      this.notesStored = this.notes;
+    }
+  },
+  watch: {
+    notesStored: function() {
+      if (this.notesStored.length > 0) {
+        this.mergeNotes();
+      }
+    }
+  }
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -5,13 +5,22 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    quotes: []
+    quotes: [],
+    notes: []
   },
   mutations: {
     GET_QUOTES: (state, quoteLoad) => {
       for (let i = 0; i < quoteLoad.length; i++) {
         state.quotes.push(quoteLoad[i]);
       }
+    },
+    GET_NOTES: (state, noteLoad) => {
+      for (let i = 0; i < noteLoad.length; i++) {
+        state.notes.push(noteLoad[i]);
+      }
+    },
+    ADD_NOTE: (state, noteAdd) => {
+      state.notes.push(noteAdd);
     }
   },
   actions: {},
