b300fec6d70dc30f1698ce0c5aa54d188474e7c1
Nick Moore <nickmoore_1@live.com>
2018-12-19 13:18:16 -0500

    Starting work on the "Yesterday" Tab

---
+++ src/components/TodoArchive.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="main">
+		<h2 class="text-grey-darkest">Test</h2>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
+
--- src/views/Yesterday.vue
+++ src/views/Yesterday.vue
@@ -3,12 +3,20 @@
     <div class="flex-1">
       <h1 class="text-5xl text-right font-sans font-thin">Yesterday</h1>
     </div>
+    <div class="flex-1 flex-col bg-grey-light m-2 p-4">
+      <todo-archive></todo-archive>
+    </div>
   </div>
 </template>
 
 <script>
+import TodoArchive from "../components/TodoArchive.vue";
+
 // @ is an alias to /src
 export default {
-  name: "yesterday"
+  name: "yesterday",
+  components: {
+    TodoArchive
+  }
 };
 </script>
